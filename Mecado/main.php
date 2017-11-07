54d83d18a834695c3532acd223e79029aee212d8
Maxime <maximesd52@gmail.com>
2017-11-07 14:56:04 +0100

    routes

--- Mecado/main.php
+++ Mecado/main.php
@@ -23,3 +23,28 @@ $db = new Illuminate\Database\Capsule\Manager();
 $db->addConnection( $config );
 $db->setAsGlobal();
 $db->bootEloquent();
+
+//MecadoView::setStyleSheet(['html/style.css']);
+MecadoView::setAppTitle('Mecado');
+
+$router = new Router();
+
+$router->addRoute('maison', '/home/', '\mecadoapp\control\MecadoController', 'viewHome');
+
+$router->addRoute('default', 'DEFAULT_ROUTE',  '\mecadoapp\control\MecadoController', 'viewHome');
+
+$router->addRoute('post', '/post/',  '\mecadoapp\control\MecadoController', 'viewPost');
+
+$router->addRoute('login', '/login/',  '\mecadoapp\control\MecadoController', 'viewLogin');
+
+$router->addRoute('check_login', '/check_login/',  '\mecadoapp\control\MecadoController', 'viewCheckLogin');
+
+$router->addRoute('check_signup', '/check_signup/',  '\mecadoapp\control\MecadoController', 'viewCreateUser');
+
+$router->addRoute('signup', '/signup/',  '\mecadoapp\control\MecadoController', 'viewSignUp');
+
+$router->addRoute('logout', '/logout/',  '\mecadoapp\control\MecadoController', 'viewlogout');
+
+$router->addRoute('send', '/send/',  '\mecadoapp\control\MecadoController', 'viewSend');
+
+$router->run();
