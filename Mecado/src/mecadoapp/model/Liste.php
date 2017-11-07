cefc35271244766c8f9c427df8e9c67a996751a0
leobob70 <leo.bernier70@hotmail.fr>
2017-11-07 15:13:59 +0100

    associations models

--- Mecado/src/mecadoapp/model/Liste.php
+++ Mecado/src/mecadoapp/model/Liste.php
@@ -4,8 +4,20 @@ namespace mecadoapp\model;
 
 class Liste extends \Illuminate\Database\Eloquent\Model {
 
-       protected $table      = 'liste'; 
-       protected $primaryKey = 'id';   
-       public    $timestamps = true;   
+    protected $table      = 'liste'; 
+    protected $primaryKey = 'id';   
+    public    $timestamps = true; 
+
+    public function messages(){
+    	return $this->hasMany(Message::class,'id_List');
+    }
+
+    public function user(){
+    	return $this->belongsTo(User::class,"id");
+    }
+
+    public function items(){
+    	return $this->belongsToMany(Liste::class,'contenir','id','id_item');
+    }
        
 }
