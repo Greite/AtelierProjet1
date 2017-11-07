ac6166737f98aa122d5bfefd506c8d6f665eb361
Maxime <maximesd52@gmail.com>
2017-11-07 14:38:44 +0100

    oui

--- Mecado/src/mecadoapp/auth/MecadoAuthentification.php
+++ Mecado/src/mecadoapp/auth/MecadoAuthentification.php
@@ -4,20 +4,6 @@ namespace mecadoapp\auth;
 
 class MecadoAuthentification extends \mf\auth\Authentification {
 
-	/*
-	 * Classe MecadoAuthentification qui définie les méthodes qui dépendent
-	 * de l'application (liée à la manipulation du modèle User) 
-	 *
-	 */
-
-	/* niveaux d'accès de MecadoApp 
-	 *
-	 * Le niveau USER correspond a un utilisateur inscrit avec un compte
-	 * Le niveau ADMIN est un plus haut niveau (non utilisé ici)
-	 * 
-	 * Ne pas oublier le niveau NONE un utilisateur non inscrit est hérité 
-	 * depuis AbstractAuthentification 
-	 */
 	const ACCESS_LEVEL_USER  = 100;   
 	const ACCESS_LEVEL_ADMIN = 200;
 
@@ -26,8 +12,7 @@ class MecadoAuthentification extends \mf\auth\Authentification {
 		parent::__construct();
 	}
 
-	public function createUser($username, $pass, $fullname,
-							   $level=self::ACCESS_LEVEL_USER) {
+	public function createUser($username, $pass, $fullname, $level=self::ACCESS_LEVEL_USER) {
 
 		$name = \mecadoapp\model\User::where('username', '=', $username)->first();
 
