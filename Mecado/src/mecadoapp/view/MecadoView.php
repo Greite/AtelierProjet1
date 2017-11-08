9a8efda20542373a0f77155370b6c33ca8340b80
Benjamin Varnerot <pamlas.bv@gmail.com>
2017-11-08 15:39:11 +0100

    base

--- Mecado/src/mecadoapp/view/MecadoView.php
+++ Mecado/src/mecadoapp/view/MecadoView.php
@@ -128,7 +128,6 @@ EOT;
 			}else{
 				$l=\mecadoapp\model\Liste::where([['destinataire', '=', $_GET['nom']],['id','=', $_GET['id']]])->first();
 				if($l!=NULL){
-					//$i=\mecadoapp\model\Liste;
 					$i=$l->items()->get();			
 					var_dump('marche');
 					$liste= <<<EOT
@@ -141,8 +140,12 @@ EOT;
 EOT;
 					foreach($i as $d){
 						$liste.= <<<EOT
-						<div></div>
+						<div>$d->nom</div>
+						<div>$d->description</div>
 						<div>$d->tarif</div>
+						<div><a href='$d->url'>Petit lien au calme</a></div>
+						<div><img src='$d->image'></div>
+						<div></div>
 EOT;
 					}
 					
