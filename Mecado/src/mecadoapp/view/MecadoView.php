4ebe644f061db0b46a365500a5dcb38e162e649b
leobob70 <leo.bernier70@hotmail.fr>
2017-11-09 16:38:43 +0100

    conflit

--- Mecado/src/mecadoapp/view/MecadoView.php
+++ Mecado/src/mecadoapp/view/MecadoView.php
@@ -73,7 +73,7 @@ EOT;
 		$prenom = $this->data->prenom;
 
 		$mail = $this->data->mail;
-		$userlists = $this->data->listes()->orderBy('date_limite', 'DESC')->get();
+		$userlists = $this->data->listes()->orderBy('date_limite','DESC')->get();
 		$profile .= <<<EOT
 				<h2>Profil</h2>	
 				<ul>
@@ -118,96 +118,142 @@ EOT;
 	}
 
 	private function renderAffichageList(){
-			$url = $_GET['id'];
-			$log = new \mecadoapp\auth\MecadoAuthentification();
-			if(is_null($url)){
-				throw new \Exception("URL invalide");
+
+		$url = $_GET['id'];
+
+		$log = new \mecadoapp\auth\MecadoAuthentification();
+		if(is_null($url)){
+			throw new \Exception("URL invalide");
+		}
+		else{
+			$liste = "";
+			date_default_timezone_set('UTC');
+
+			$list=\mecadoapp\model\Liste::where('url','=', $url)->first();
+			$user=\mecadoapp\model\User::where('mail','=', $_SESSION['user_login'])->first();
+
+			if(date('Y-m-d')==$list->date_limite){
+				$message = \mecadoapp\model\Message::select()->where([["id_liste","=",$list->id],["type","=",0]])->get();
+				$item= \mecadoapp\model\Item::where([["id_liste","=",$list->id],["reserver","=",1]])->get();
+
+				foreach($message as $value){
+					$author=$value->auteur;
+					$text=$value->description;
+
+					$liste .=<<<EOT
+					<div>
+						<label>$author</label><p>$text</p>
+						<p>salut</p>
+					</div>
+EOT;
+				}
+
+				foreach ($item as $v){
+					$liste .="
+					<div>
+						<p>".$v->nom."</p>
+						<p>Offert par : ".$v->reserviste." </p>
+						<img src=".$v->image.">
+					</div>";
+				}
+				return $liste;
 			}else{
-
-				$list=\mecadoapp\model\Liste::where('url','=', $url)->first();
-				$user=\mecadoapp\model\User::where('mail','=', $_SESSION['user_login'])->first();
-
 				if(!is_null($list)){
-					$_SESSION['liste']=$list->id;
-					$i=$list->items()->get();
-					$liste= <<<EOT
-					<article>
+					if(date('Y-m-d')==$list->liste){
+						foreach($message as $value){
+							$author=$value->auteur;
+							$text=$value->description;
+							$liste .= <<<EOT
+							<div>
+							<label>$author</label><p>$text</p>
+							<p>salut</p>
+							</div>
+EOT;
+						}
+					}else{
+						$_SESSION['liste']=$list->id;
+						$i=$list->items()->get();
+						$liste= <<<EOT
+						<article>
 						<h1>$list->titre</h1>
 						<label>Destinataire : <p>$list->destinataire</p></label>
 						<label>Date limite : <p>$list->date_limite</p></label>
 						<label>Description : <p>$list->description</p></label>
 						<a href="$this->script_name/ajoutitem/?id=$url"><input type="button" placeholder="Ajouter un item"></a>
-					</article>
-						
+						</article>
+
 EOT;
-					if ($log->logged_in && ($list->id_user == $user->id)) {
-						$liste.= <<<EOT
+						if ($log->logged_in && ($list->id_user == $user->id)) {
+							$liste.= <<<EOT
 							<a href="$this->script_name/ajoutitem/?id=$url"><input type="button" name="Ajouter un item" value="Ajouter un item"></a>
 EOT;
-					}	
-					$liste .= "</article>";
-
-					foreach($i as $d){
-						$liste.= <<<EOT
-						<article>
+						}	
+						$liste .= "</article>";
+						foreach($i as $d){
+							$liste.= <<<EOT
+							<article>
 							<h2>$d->nom</h2>
 							<span><h3>Prix : </h3><h3>$d->tarif €</h3></span>
 							<label>Description : <p>$d->description</p></label>
 EOT;
-						if (!is_null($d->url)) {
-							$liste.= <<<EOT
-							<div><a href='$d->url' target = '_blank'>Lien du cadeau</a></div>
-EOT;
-						}
-						if (!is_null($d->image)) {
-							$liste.= <<<EOT
-						<div><img src='$d->image'></div>
-						</article>
-EOT;
-						}
-						if ($list->for_other) {
-							if($d->reserver==0){
-							$liste.= <<<EOT
-							<form action='$this->script_name/reserve/?id=$url' method='post'>
-								<label for="reserviste">Votre nom : </label>
-								<input type="text" id="reserviste" name="reserviste">
-								<label for="message">Laisser lui un message : </label>
-								<input type="text" id="message" name="message">
-								<input type="hidden" id="id" name="id" value="$d->id">
-								<input type="submit" id="send" value="Réserver">
-							</form>
-EOT;
-							}else{
+							if (!is_null($d->url)) {
 								$liste.= <<<EOT
-								<div>Réserver par $d->reserviste</div>							
+								<div><a href='$d->url' target = '_blank'>Lien du cadeau</a></div>
 EOT;
 							}
-						}
-						if (!$log->logged_in) {
-							if($d->reserver==0){
-							$liste.= <<<EOT
-							<form action='$this->script_name/reserve/?id=$url' method='post'>
-								<label for="reserviste">Votre nom : </label>
-								<input type="text" id="reserviste" name="reserviste">
-								<label for="message">Laisser lui un message : </label>
-								<input type="text" id="message" name="message">
-								<input type="hidden" id="id" name="id" value="$d->id">
-								<input type="submit" id="send" value="Réserver">
-							</form>
-EOT;
-							}else{
+							if (!is_null($d->image)) {
 								$liste.= <<<EOT
-								<div>Réserver par $d->reserviste</div>							
+								<div><img src='$d->image'></div>
+								</article>
 EOT;
+							}
+							if ($list->for_other) {
+								if($d->reserver==0){
+										$liste.= <<<EOT
+										<form action='$this->script_name/reserve/?id=$url' method='post'>
+											<label for="reserviste">Votre nom : </label>
+											<input type="text" id="reserviste" name="reserviste">
+											<label for="message">Laisser lui un message : </label>
+											<input type="text" id="message" name="message">
+											<input type="hidden" id="id" name="id" value="$d->id">
+											<input type="submit" id="send" value="Réserver">
+										</form>
+EOT;
+								}else{
+									$liste.= <<<EOT
+									<div>Réserver par $d->reserviste</div>							
+EOT;
+								}
+							}
+							if (!$log->logged_in) {
+								if($d->reserver==0){
+									$liste.= <<<EOT
+									<form action='$this->script_name/reserve/?id=$url' method='post'>
+										<label for="reserviste">Votre nom : </label>
+										<input type="text" id="reserviste" name="reserviste">
+										<label for="message">Laisser lui un message : </label>
+										<input type="text" id="message" name="message">
+										<input type="hidden" id="id" name="id" value="$d->id">
+										<input type="submit" id="send" value="Réserver">
+									</form>
+EOT;
+								}else{
+									$liste.= <<<EOT
+									<div>Réserver par $d->reserviste</div>							
+EOT;
+								}
 							}
 						}
 					}
-				return $liste;
+					return $liste;
 				}else{
 					throw new \Exception("URL VIDE !");
 				}
-			}
+			}	
+		}
 	}
+
+
 
 	private function renderLogin(){
 		$login = <<<EOT
