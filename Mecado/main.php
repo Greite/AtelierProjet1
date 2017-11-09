5929a8d6e9788e6e6e700878dbe069e1b9ecdc1c
Julien <charlesj52130@gmail.com>
2017-11-09 09:52:55 +0100

    level

--- Mecado/main.php
+++ Mecado/main.php
@@ -26,38 +26,38 @@ MecadoView::setAppTitle('Mecado');
 
 $router = new Router();
 
-$router->addRoute('maison', '/home/', '\mecadoapp\control\MecadoController', 'viewHome');
+$router->addRoute('maison', '/home/', '\mecadoapp\control\MecadoController', 'viewHome', mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('default', 'DEFAULT_ROUTE',  '\mecadoapp\control\MecadoController', 'viewHome');
+$router->addRoute('default', 'DEFAULT_ROUTE',  '\mecadoapp\control\MecadoController', 'viewHome', mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('post', '/post/',  '\mecadoapp\control\MecadoController', 'viewPost');
+$router->addRoute('post', '/post/',  '\mecadoapp\control\MecadoController', 'viewPost', mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('login', '/login/',  '\mecadoapp\control\MecadoController', 'viewLogin');
+$router->addRoute('login', '/login/',  '\mecadoapp\control\MecadoController', 'viewLogin', mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('check_login', '/check_login/',  '\mecadoapp\control\MecadoController', 'viewCheckLogin');
+$router->addRoute('check_login', '/check_login/',  '\mecadoapp\control\MecadoController', 'viewCheckLogin', mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('check_signup', '/check_signup/',  '\mecadoapp\control\MecadoController', 'viewCreateUser');
+$router->addRoute('check_signup', '/check_signup/',  '\mecadoapp\control\MecadoController', 'viewCreateUser',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('signup', '/signup/',  '\mecadoapp\control\MecadoController', 'viewSignUp');
+$router->addRoute('signup', '/signup/',  '\mecadoapp\control\MecadoController', 'viewSignUp',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('logout', '/logout/',  '\mecadoapp\control\MecadoController', 'viewlogout');
+$router->addRoute('logout', '/logout/',  '\mecadoapp\control\MecadoController', 'viewlogout',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('send', '/send/',  '\mecadoapp\control\MecadoController', 'viewSendMessage');
+$router->addRoute('send', '/send/',  '\mecadoapp\control\MecadoController', 'viewSendMessage',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('messages', '/messages/',  '\mecadoapp\control\MecadoController', 'viewMessages');
+$router->addRoute('messages', '/messages/',  '\mecadoapp\control\MecadoController', 'viewMessages',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('createlist', '/createlist/',  '\mecadoapp\control\MecadoController', 'viewCreateList');
+$router->addRoute('createlist', '/createlist/',  '\mecadoapp\control\MecadoController', 'viewCreateList',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('createurl', '/createurl/',  '\mecadoapp\control\MecadoController', 'viewCreateUrl');
+$router->addRoute('createurl', '/createurl/',  '\mecadoapp\control\MecadoController', 'viewCreateUrl',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('profile', '/profile/',  '\mecadoapp\control\MecadoController', 'viewProfile');
+$router->addRoute('profile', '/profile/',  '\mecadoapp\control\MecadoController', 'viewProfile',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('check_createlist', '/check_createlist/',  '\mecadoapp\control\MecadoController', 'viewCheckCreateList');
+$router->addRoute('check_createlist', '/check_createlist/',  '\mecadoapp\control\MecadoController', 'viewCheckCreateList',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('ajout_item', '/ajoutitem/',  '\mecadoapp\control\MecadoController', 'viewAjoutItem');
+$router->addRoute('ajout_item', '/ajoutitem/',  '\mecadoapp\control\MecadoController', 'viewAjoutItem',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-$router->addRoute('affichage_list', '/affichagelist/',  '\mecadoapp\control\MecadoController', 'viewAffichageList');
+$router->addRoute('affichage_list', '/affichagelist/',  '\mecadoapp\control\MecadoController', 'viewAffichageList',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
 
-$router->addRoute('save_item','/saveitem/', '\mecadoapp\control\MecadoController','viewSaveItem');
+$router->addRoute('save_item','/saveitem/', '\mecadoapp\control\MecadoController','viewSaveItem',mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
 $router->run();
